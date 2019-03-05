4024644ccba498417154deda2dc5da9f46b566ed
João <contato@joaoprado.com.br>
2019-03-05 00:16:34 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-echo 'joaoprado.com.br' > CNAME
+# if you are deploying to a custom domain
+# echo 'joaoprado.com.br' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+ git push -f git@github.com:joaoprado/joaoprado.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
