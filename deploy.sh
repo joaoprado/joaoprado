24024cfebefd65b6dd0a93340aa6ac829c5da44b
João <contato@joaoprado.com.br>
2019-03-11 10:48:17 -0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,9 @@ set -e
 # build
 docker exec -it dev_workspace_1 bash -c "cd joaoprado && npm run build"
 docker exec -it dev_workspace_1 bash -c "cd joaoprado/dist && echo 'joaoprado.com.br' > CNAME"
+docker exec -it dev_workspace_1 bash -c "cd joaoprado/dist && git init && git add -A && git commit -m 'deploy'"
 
-git init
-git add -A
-git commit -m 'deploy'
+cd dist
 
 git push -f git@github.com:joaoprado/joaoprado.github.io.git master
 
